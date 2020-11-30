4b12ca7b6158d6e99ea3fef96648ae7b7667a24a
Artur Côrte-Real <artur.corte-real@nfon.com>
2020-11-30 11:38:46 +0000

    Added query class

---
+++ src/app/state/query.ts
@@ -0,0 +1,22 @@
+import { TodoState, TodoStore } from './store';
+import { Query } from '@datorama/akita';
+import { Observable } from 'rxjs';
+import { Todo } from '../todo.model';
+
+export class TodoQuery extends Query<TodoState> {
+  constructor(private todoStore: TodoStore) {
+    super(todoStore);
+  }
+
+  getTodos(): Observable<Todo[]> {
+    return this.select((state) => state.todos);
+  }
+
+  getLoaded(): Observable<boolean> {
+    return this.select((state) => state.isLoaded);
+  }
+
+  getLoading(): Observable<boolean> {
+    return this.selectLoading();
+  }
+}
